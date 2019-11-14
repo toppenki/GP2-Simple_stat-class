995205b56627ea95ab3e06c4b0b3d8209ca3e2c6
toppenki <55781143+toppenki@users.noreply.github.com>
2019-11-13 22:32:29 -0800

    Creating Simple_stat.h File
    bare bones to get started, includes all the simple functions that provide us with the required return values

---
+++ Simple_stat.h
@@ -0,0 +1,55 @@
+#include <iostream>
+
+template <typename I>
+class Simple_stat {
+
+private:
+I max_val = 0; //maximum
+I min_val = 0; //minimum
+I mode = 0; //most repeated #
+I median = 0; //middle value
+I mean = 0; //avg
+I std_dev = 0; //standard deviation
+
+//NEED CONSTRUCTOR
+//NEED DESTRUCTOR
+
+//calculating functions
+I find_mode() {
+
+}
+
+I find_median() {
+
+}
+
+I find_mean() {
+
+}
+
+I find_SD() {
+
+}
+
+I find_min() {
+
+}
+
+I find_max() {
+
+}
+
+
+public:
+
+//getters
+I get_mode() { return mode }
+I get_median() { return median }
+I get_mean() { return mean }
+I get_SD() { return std_dev }
+I get_min { return min_val }
+I get_max { return max_val }
+
+
+
+}
